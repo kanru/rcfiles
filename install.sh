1f3e4206532ed9ea4bb0d31b3a9e77fe4c87d552
Kan-Ru Chen <kanru@kanru.info>
2012-08-08 07:10:39 +0800

    Backup instead of force remove

--- install.sh
+++ install.sh
@@ -9,18 +9,18 @@ git=$SCRIPTPATH
 cd ~
 
 # zsh
-ln -fs $git/zsh/zsh       .zsh
-ln -fs $git/zsh/zshrc     .zshrc
-ln -fs $git/zsh/zshenv    .zshenv
-ln -fs $git/zsh/zshalias  .zshalias
-ln -fs $git/zsh/zshhashes .zshhashes
+ln -bs $git/zsh/zsh       .zsh
+ln -bs $git/zsh/zshrc     .zshrc
+ln -bs $git/zsh/zshenv    .zshenv
+ln -bs $git/zsh/zshalias  .zshalias
+ln -bs $git/zsh/zshhashes .zshhashes
 
 # others
-ln -fs $git/Xresources .Xresources
-ln -fs $git/cvsrc      .cvsrc
-ln -fs $git/devscripts .devscripts
-ln -fs $git/fonts.conf .fonts.conf
-ln -fs $git/gitconfig  .gitconfig
-ln -fs $git/hgrc       .hgrc
-ln -fs $git/nethackrc  .nethackrc
-ln -fs $git/quiltrc    .quiltrc
+ln -bs $git/Xresources .Xresources
+ln -bs $git/cvsrc      .cvsrc
+ln -bs $git/devscripts .devscripts
+ln -bs $git/fonts.conf .fonts.conf
+ln -bs $git/gitconfig  .gitconfig
+ln -bs $git/hgrc       .hgrc
+ln -bs $git/nethackrc  .nethackrc
+ln -bs $git/quiltrc    .quiltrc
